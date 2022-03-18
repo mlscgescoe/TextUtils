4deaa822beef27d947009632b66ea870a30bfadc
Arya <aryadanech9@gmail.com>
2022-03-18 16:42:12 +0530

    Added Dark Mode Feature

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import About from './components/About';
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
         aboutText = "About TextUtils"/>
 
       <div className="container my-3">
-        <TextForm heading = "Enter Text to Analyze"/>
+        {/* <TextForm heading = "Enter Text to Analyze"/> */}
+        <About/>
       </div>
     </>
   );
